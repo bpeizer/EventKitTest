731dcca681aebf150209c3fdcebd7cdfd00d5fad
bpeizer <bpeizer@imagemattersllc.com>
2018-01-24 19:29:54 -0500

    Updated Test

--- tests/04Download DataPack.js
+++ tests/04Download DataPack.js
@@ -56,7 +56,7 @@ var https = require('https');
        .click(`.qa-BreadcrumbStepper-NavigationCheck`, `CSS`, `Click element`)
        .click(`.qa-BreadcrumbStepper-NavigationCheck`, `CSS`, `Click element`)
        .pause(60000)
-       .waitForElementPresent(`div > :nth-child(3) .qa-ProviderRow-Check-providerStatus`,120000)
+       .waitForElementPresent(`div > :nth-child(3) .qa-ProviderRow-Check-providerStatus`,240000)
        .click(`.qa-ProviderRow-ArrowDown`, `CSS`, `Click element`)
        .click(`.qa-ProviderRow-ArrowDown`, `CSS`, `Click element`)
        .click(`div:nth-of-type(2) > div:nth-of-type(4) > div > div:nth-of-type(3) > div:nth-of-type(2) > table > tbody > tr > td:nth-of-type(2) > svg > path`, `CSS`, `Click element`)
