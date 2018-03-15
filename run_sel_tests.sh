8fb8679d5e2d7f4c84666b57fc5708ed4c9f29ad
bpeizer <bpeizer@imagemattersllc.com>
2018-03-15 11:58:44 -0400

    Updated to use non parallel command

--- run_sel_tests.sh
+++ run_sel_tests.sh
@@ -26,7 +26,7 @@ echo "RUN TESTS ON FIREFOX"
 
 		# Run the Firefox Selenium tests.
 		FirefoxContainer="$(docker run -d -p 4444:4444 -p 5900:5900 -v /dev/shm:/dev/shm selenium/standalone-firefox-debug:3.11.0-antimony)"  
-		npm testFirefox || { latch=1; }
+		./node_modules/.bin/nightwatch -e firefox tests || { latch=1; }
 		# Remember that there was an overall failure, if a single iteration has a failure.
 		docker logs "${FirefoxContainer}"
 		docker kill "${FirefoxContainer}"
@@ -36,7 +36,7 @@ echo "RUN TESTS ON FIREFOX"
 echo "RUN TESTS ON CHROME"
 		# Run the Chrome Selenium tests.
 		ChromeContainer="$(docker run -d -p 4444:4444 -p 5900:5900 -v /dev/shm:/dev/shm selenium/standalone-chrome-debug:3.11.0-antimony)"  
-		npm testChrome || { latch=1; }
+		./node_modules/.bin/nightwatch -e chrome tests || { latch=1; }
 		docker logs "${ChromeContainer}"
 		docker kill "${ChromeContainer}"
 		# Remember that there was an overall failure, if a single iteration has a failure.
